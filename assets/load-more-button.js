1f9683557725dd95530824d507e1a61c74fc6582
Mark Wong <mark.wong@omaze.com>
2024-05-31 19:57:16 +1000

    removed jquery added section rendering api

--- assets/load-more-button.js
+++ assets/load-more-button.js
@@ -5,30 +5,39 @@ document.addEventListener('DOMContentLoaded', function () {
   let paginationList = document.getElementsByClassName('pagination__list')[0];
   loadMoreBtn.addEventListener('click', function (e) {
     e.preventDefault();
-    $.ajax({
-      url: nextUrl,
-      type: 'GET',
-      dataType: 'html',
-      success: function (nextPage) {
-        window.history.pushState('', '', nextUrl);
-        let newProducts = $(nextPage).find('#product-grid');
-        productList.innerHTML += newProducts.html();
-        let newUrl = $(nextPage).find('.button--load-more').attr('href');
+
+    function handleResponse() {
+      var parser = new DOMParser();
+      window.history.pushState('', '', nextUrl);
+      var nextPageHTML = parser.parseFromString(this.responseText, 'text/html');
+      //console.log(nextPageHTML);
+      let newProducts = nextPageHTML.getElementById('product-grid');
+      productList.innerHTML += newProducts.innerHTML;
+      let nextPageLoadMore = nextPageHTML.getElementsByClassName('button--load-more')[0];
+      if (nextPageLoadMore) {
+        let newUrl = nextPageLoadMore.getAttribute('href');
         if (newUrl) {
           nextUrl = newUrl;
           loadMoreBtn.href = newUrl;
         } else {
           loadMoreBtn.style.display = 'none';
         }
-        let currentLink = paginationList.querySelectorAll('.pagination__item--current')[0];
-        currentLink.classList.remove('pagination__item--current');
-        currentLink.setAttribute('aria-disabled', 'false');
-        currentLink.setAttribute('href', currentLink.getAttribute('data-link'));
-        let nextLink = currentLink.parentElement.nextSibling.firstChild;
-        nextLink.classList.add('pagination__item--current');
-        nextLink.setAttribute('aria-disabled', 'true');
-        nextLink.removeAttribute('href');
-      },
-    });
+      } else {
+        loadMoreBtn.style.display = 'none';
+      }
+      let currentLink = paginationList.querySelectorAll('.pagination__item--current')[0];
+      currentLink.classList.remove('pagination__item--current');
+      currentLink.setAttribute('aria-disabled', 'false');
+      currentLink.setAttribute('href', currentLink.getAttribute('data-link'));
+      let nextLink = currentLink.parentElement.nextSibling.firstChild;
+      nextLink.classList.add('pagination__item--current');
+      nextLink.setAttribute('aria-disabled', 'true');
+      nextLink.removeAttribute('href');
+    }
+    const request = new XMLHttpRequest();
+    request.addEventListener('load', handleResponse);
+    //console.log(nextUrl);
+    request.open('GET', nextUrl + '&section_id=template--22694769918259__product-grid', true);
+    request.send();
   });
 });
